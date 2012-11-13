71a17caf655540d8fb1d2f4f1cadcfd04b994684
Ben Carney <ben@carnivorous.org>
2012-11-12 20:29:01 -0800

    Added first draft of accelerometer driven android application interface

--- interfaces/web/mobile/assets/www/js/index.js
+++ interfaces/web/mobile/assets/www/js/index.js
@@ -1,49 +1,59 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 var app = {
+    currentCommands: [],
     // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
     // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('deviceready', app.onDeviceReady);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        var options = { frequency: 500 };  // Update every .5 seconds
+        var watchID = navigator.accelerometer.watchAcceleration(app.onAccelSucces, app.onAccelError, options);
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    onAccelSucces: function(acceleration){
+        if(acceleration.y < -3 && $.inArray('l', app.currentCommands) < 0){
+            $('#status').html('left');
+            app.currentCommands.push('l');
+            app.currentCommands.splice($.inArray('r', app.currentCommands), 1);
+            app.currentCommands.splice($.inArray('c', app.currentCommands), 1);
+            $.ajax({
+                url: 'http://192.168.0.104:4567',
+                data: { command: 'l'}
+            })
+        }
+        if(acceleration.y > 3 && $.inArray('l', app.currentCommands) < 0){
+            $('#status').html('right');
+            app.currentCommands.push('r');
+            app.currentCommands.splice($.inArray('l', app.currentCommands), 1);
+            app.currentCommands.splice($.inArray('c', app.currentCommands), 1);
+            $.ajax({
+                url: 'http://192.168.0.104:4567',
+                data: { command: 'r'}
+            })
+        }
+        if(acceleration.y >= -3 && acceleration.y <= 3 && $.inArray('c', app.currentCommands) < 0){
+            $('#status').html('center');
+            app.currentCommands.push('c');
+            app.currentCommands.splice($.inArray('l', app.currentCommands), 1);
+            app.currentCommands.splice($.inArray('r', app.currentCommands), 1);
+            $.ajax({
+                url: 'http://192.168.0.104:4567',
+                data: { command: 'c'}
+            })
+        }
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+        document.querySelector('#x').innerHTML = acceleration.x;
+        document.querySelector('#y').innerHTML = acceleration.y;
+        document.querySelector('#z').innerHTML = acceleration.z;
+    },
+    onAccelError: function() {
+        alert('onError!');
     }
 };
+
+
+
